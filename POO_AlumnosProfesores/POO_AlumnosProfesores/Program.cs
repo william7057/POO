ab42e02d92b92102cd1b11ea2ac74507851a20b7
william <william7057@gmail.com>
2019-09-14 16:06:05 -0500

    Iniciando POO

---
+++ POO_AlumnosProfesores/POO_AlumnosProfesores/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_AlumnosProfesores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("GESTION DE INSTITUCION");
+            Console.ReadLine();
+            }
+    }
+}
